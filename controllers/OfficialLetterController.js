03a64dcfb1eeea4dd347bb49192c5e59a49e1abe
reinhardjk14 <reinhardjk14@gmail.com>
2022-08-13 20:00:32 +0700

    adding pagination for reimbursements and official letters

--- controllers/OfficialLetterController.js
+++ controllers/OfficialLetterController.js
@@ -3,10 +3,21 @@ const { OfficialLetter, Reimbursement } = require("../models");
 class OfficialLetterController {
   static async getAllOfficialLetter(req, res, next) {
     try {
-      const response = await OfficialLetter.findAll({
-        include: [Reimbursement]
+      const { page: xpage, size } = req.query;
+      const page = xpage - 1;
+      const limit = size ? +size : 9;
+      const offset = page ? +page * limit : 0;
+      const response = await OfficialLetter.findAndCountAll({
+        limit,
+        offset,
+        include: [Reimbursement],
       });
-      res.status(200).json(response);
+      res.status(200).json({
+        totalOfficialLetters: response.count,
+        totalPages: Math.ceil(response.count / size),
+        currentPage: +xpage,
+        response,
+      });
     } catch (err) {
       next(err);
     }
--- controllers/ReimbursementController.js
+++ controllers/ReimbursementController.js
@@ -3,8 +3,20 @@ const { Reimbursement } = require("../models");
 class ReimbursementController {
   static async getReimbursements(req, res, next) {
     try {
-      const response = await Reimbursement.findAll();
-      res.status(200).json(response);
+      const { page: xpage, size } = req.query;
+      const page = xpage - 1;
+      const limit = size ? +size : 9;
+      const offset = page ? +page * limit : 0;
+      const response = await Reimbursement.findAndCountAll({
+        limit,
+        offset,
+      });
+      res.status(200).json({
+        totalReimbursements: response.count,
+        totalPages: Math.ceil(response.count / size),
+        currentPage: +xpage,
+        response,
+      });
     } catch (err) {
       next(err);
     }

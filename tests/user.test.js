ff7ae69800eff9e081d0b244ea290b41cbd283ca
Teresa Magira <54297926+tmagira@users.noreply.github.com>
2022-08-16 14:17:27 +0700

    User Tester Done

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,8 +1,6 @@
 const request = require("supertest");
 const app = require("../App");
 const { sequelize, User } = require("../models");
-const { queryInterface } = sequelize;
-const pass = require("../helpers/bcrypt");
 const { signPayload } = require("../helpers/jwt");
 
 const generateToken = () => {
@@ -20,6 +18,7 @@ const generateToken = () => {
 let dummyUser = null;
 let dummyAdmin = null;
 let adminToken = null;
+let userToken = null;
 
 beforeAll((done) => {
   let userData = {
@@ -34,19 +33,18 @@ beforeAll((done) => {
   };
 
   let data = {
-    firstName: "test",
-    lastName: "Puter",
-    role: "Super Admin",
-    email: "test@dinasq.com",
+    firstName: "Hermiyon",
+    lastName: "Granjer",
+    role: "Staff",
+    email: "hermiyon@dinasq.com",
     password: "12341234",
     phoneNumber: "081212121212",
     address: "Jl. Hokwart No. 13",
-    position: "Software Engineer",
+    position: "Region Manager",
   };
 
   User.create(userData)
     .then((user) => {
-      
       dummyAdmin = user.dataValues;
       adminToken = signPayload({
         id: dummyAdmin.id,
@@ -57,12 +55,21 @@ beforeAll((done) => {
     })
     .then((user) => {
       dummyUser = user.dataValues;
+      userToken = signPayload({
+        id: dummyUser.id,
+        email: dummyUser.email,
+        role: dummyUser.role,
+      });
       done();
     })
     .catch((err) => {
       done(err);
       console.log(err);
     });
+});
+
+beforeEach(() => {
+  jest.restoreAllMocks();
 });
 
 afterAll((done) => {
@@ -94,19 +101,19 @@ describe("POST /login - Admin Login Test", () => {
   });
 
   describe("POST /login - Admin Login - Fail Test", () => {
-    it("Should return a status of 401 and an unathorized message", async () => {
-      let userData = {
-        firstName: "Hermiyon",
-        lastName: "Genjer",
-        role: "Staff",
-        email: "hermiyon@dinasq.com",
-        password: "12341234",
-        phoneNumber: "081212121212",
-        address: "Jl. Hokwart No. 13",
-        position: "Software Engineer",
+    it("Should return a status of 404 and a not found message", async () => {
+      const payload = {
+        email: "heri@dinasq.com",
+        password: "1234123456",
       };
-      await User.create(userData);
 
+      const res = await request(app).post("/login").send(payload);
+
+      expect(res.status).toBe(404);
+      expect(res.body).toHaveProperty("message", "User not found");
+    });
+
+    it("Should return a status of 401 and an unathorized message", async () => {
       const payload = {
         email: "hermiyon@dinasq.com",
         password: "12341234",
@@ -127,20 +134,8 @@ describe("POST /login - Admin Login Test", () => {
 describe("POST /login-all - All Users Login Test", () => {
   describe("POST /login-all - All Users Login - Success Test", () => {
     it("Should return a status of 200, a success message, and an access token", async () => {
-      let userData = {
-        firstName: "Blake",
-        lastName: "Lively",
-        role: "Staff",
-        email: "blake@dinasq.com",
-        password: "12341234",
-        phoneNumber: "081212121212",
-        address: "Jl. Hokwart No. 13",
-        position: "Software Engineer",
-      };
-      await User.create(userData);
-
       const payload = {
-        email: "blake@dinasq.com",
+        email: "hermiyon@dinasq.com",
         password: "12341234",
       };
 
@@ -157,9 +152,9 @@ describe("POST /login-all - All Users Login Test", () => {
 describe("GET /users", () => {
   describe("GET /users - Get All Users Data - Success Test", () => {
     it("Should return all users", async () => {
-      const access_token = generateToken();
-
-      const res = await request(app).get("/users").set({ access_token });
+      const res = await request(app)
+        .get("/users")
+        .set({ access_token: adminToken });
 
       expect(res.status).toBe(200);
     });
@@ -170,9 +165,9 @@ describe("GET /users", () => {
 describe("GET /usersdetails", () => {
   describe("GET /usersetails - Get All Users Details Data - Success Test", () => {
     it("Should return all users details", async () => {
-      const access_token = generateToken();
-
-      const res = await request(app).get("/usersdetails").set({ access_token });
+      const res = await request(app)
+        .get("/usersdetails")
+        .set({ access_token: adminToken });
 
       expect(res.status).toBe(200);
       expect(res.body[0]).toHaveProperty("OfficialLetters");
@@ -184,13 +179,13 @@ describe("GET /usersdetails", () => {
 describe("GET /users/:id", () => {
   describe("GET /users/:id - Get One User Data - Success Test", () => {
     it("Should return a user data", async () => {
-      const access_token = generateToken();
-
-      const res = await request(app).get("/users/1").set({ access_token });
+      const res = await request(app)
+        .get("/users/" + dummyUser.id)
+        .set({ access_token: adminToken });
 
       expect(res.status).toBe(200);
-      expect(res.body).toHaveProperty("id", 1);
-      expect(res.body).toHaveProperty("firstName", "Heri");
+      expect(res.body).toHaveProperty("id", 2);
+      expect(res.body).toHaveProperty("firstName", "Hermiyon");
       expect(res.body).toHaveProperty("OfficialLetters");
     });
   });
@@ -200,11 +195,9 @@ describe("GET /users/:id", () => {
 describe("GET /logged-in-user", () => {
   describe("GET /logged-in-user - Get Logged In User Data - Success Test", () => {
     it("Should return the logged in user data", async () => {
-      const access_token = generateToken();
-
       const res = await request(app)
         .get("/logged-in-user")
-        .set({ access_token });
+        .set({ access_token: adminToken });
 
       expect(res.status).toBe(200);
       expect(res.body).toHaveProperty("id", 1);
@@ -212,14 +205,25 @@ describe("GET /logged-in-user", () => {
       expect(res.body).toHaveProperty("OfficialLetters");
     });
   });
+
+  describe("GET /logged-in-user - Get Logged In User Data - Fail Test", () => {
+    it("Should return a status of 500 and an ISE message", async () => {
+        jest.spyOn(User, "findByPk").mockRejectedValue("Error");
+        const res = await request(app)
+        .get("/logged-in-user")
+        .set({ access_token: adminToken });
+
+        expect(res.status).toBe(500);
+        expect(res.body).toHaveProperty("message", "Internal server error");
+    });
+  });
 });
 
 //UPDATE USER DETAILS
 describe("PUT /users/:id", () => {
-  describe("PUT /users/:id - Update User Details - Success Test", () => {
-    it("Should return a status of 200 and a success message", async () => {
-      const access_token = generateToken();
-
+  describe("PUT /users/:id - Update User Details - Fail Test", () => {
+    it("Should return a status of 500 and an ISE message", async () => {
+      jest.spyOn(User, "update").mockRejectedValue("Error");
       const payload = {
         firstName: "Update",
         lastName: "Newton",
@@ -232,32 +236,104 @@ describe("PUT /users/:id", () => {
       };
 
       const res = await request(app)
-        .put("/users/2")
+        .put("/users/" + dummyUser.id)
         .send(payload)
-        .set({ access_token });
+        .set({ access_token: adminToken });
+
+        expect(res.status).toBe(500);
+        expect(res.body).toHaveProperty("message", "Internal server error");
+    });
+  });
+
+  describe("PUT /users/:id - Update User Details - Success Test", () => {
+    it("Should return a status of 200 and a success message", async () => {
+      const payload = {
+        firstName: "Update",
+        lastName: "Newton",
+        role: "Super Admin",
+        email: "newton@dinasq.com",
+        password: "12341234",
+        phoneNumber: "081212121212",
+        address: "Jl. Cempaka No. 12",
+        position: "Backend Engineer",
+      };
+
+      const res = await request(app)
+        .put("/users/" + dummyUser.id)
+        .send(payload)
+        .set({ access_token: adminToken });
 
       expect(res.status).toBe(200);
       expect(res.body).toHaveProperty(
         "message",
-        "Successfully updating user 2"
+        "Successfully updating user " + dummyUser.id
       );
+    });
+  });
+});
+
+//UPDATE PASSWORD FOR STAFF
+describe("PATCH /users/:id", () => {
+  describe("PATCH /users/:id - Update Staff User Password - Fail Test", () => {
+    it("Should return a status of 500 and an ISE message", async () => {
+      jest.spyOn(User, "update").mockRejectedValue("Error");
+      const payload = {
+        password: "6789067890",
+      };
+
+      const res = await request(app)
+        .patch("/users")
+        .send(payload)
+        .set({ access_token: adminToken });
+
+      expect(res.status).toBe(500);
+      expect(res.body).toHaveProperty("message", "Internal server error");
+    });
+  });
+
+  describe("PATCH /users/:id - Update Staff User Password - Success Test", () => {
+    it("Should return a user data", async () => {
+      const payload = {
+        password: "6789067890",
+      };
+
+      const res = await request(app)
+        .patch("/users")
+        .send(payload)
+        .set({ access_token: adminToken });
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("message", "Password has been updated");
     });
   });
 });
 
 //DELETE USER
 describe("DELETE /users/:id", () => {
-  describe("DELETE /users/:id - Delete a User - Success Test", () => {
-    it("Should return a user data", async () => {
-      const access_token = generateToken();
+  describe("DELETE /users/:id - Delete a User - Fail Test", () => {
+    it("Should return a status of 500 and an ISE message", async () => {
+      jest.spyOn(User, "destroy").mockRejectedValue("Error");
 
-      console.log(adminToken, "<<<<< admin tkn")
+      const res = await request(app)
+        .delete("/users/" + dummyUser.id)
+        .set({ access_token: adminToken });
+
+      expect(res.status).toBe(500);
+      expect(res.body).toHaveProperty("message", "Internal server error");
+    });
+  });
+
+  describe("DELETE /users/:id - Delete a User - Success Test", () => {
+    it("Should return a status of 200 and a success message", async () => {
       const res = await request(app)
         .delete("/users/" + dummyUser.id)
         .set({ access_token: adminToken });
 
       expect(res.status).toBe(200);
-      expect(res.body).toHaveProperty('message', 'Successfully deleting user 2')
+      expect(res.body).toHaveProperty(
+        "message",
+        "Successfully deleting user 2"
+      );
     });
   });
 });
@@ -266,14 +342,6 @@ describe("DELETE /users/:id", () => {
 describe("POST /register - Create User", () => {
   describe("POST /register - Create User - Success Test", () => {
     it("Should return a status of 201 with a success message", async () => {
-      const jwtPayload = {
-        id: 1,
-        email: "heri@dinasq.com",
-        role: "Super Admin",
-      };
-
-      const access_token = signPayload(jwtPayload);
-
       const payload = {
         firstName: "Budi",
         lastName: "Budiman",
@@ -288,7 +356,7 @@ describe("POST /register - Create User", () => {
       const res = await request(app)
         .post("/register")
         .send(payload)
-        .set({ access_token });
+        .set({ access_token: adminToken });
       expect(res.status).toBe(201);
       expect(res.body).toHaveProperty(
         "message",
@@ -299,14 +367,6 @@ describe("POST /register - Create User", () => {
 
   describe("POST /register - fail test", () => {
     it('Should return a status of 400 and a message of "Email is Required"', async () => {
-      const jwtPayload = {
-        id: 1,
-        email: "heri@dinasq.com",
-        role: "Super Admin",
-      };
-
-      const access_token = signPayload(jwtPayload);
-
       const payload = {
         firstName: "Budi",
         lastName: "Budiman",
@@ -321,20 +381,12 @@ describe("POST /register - Create User", () => {
       const res = await request(app)
         .post("/register")
         .send(payload)
-        .set({ access_token });
+        .set({ access_token: adminToken });
       expect(res.status).toBe(400);
       expect(res.body).toHaveProperty("message", "Please insert email");
     });
 
     it('Should return a status 400 and a message of "Password is Required"', async () => {
-      const jwtPayload = {
-        id: 1,
-        email: "heri@dinasq.com",
-        role: "Super Admin",
-      };
-
-      const access_token = signPayload(jwtPayload);
-
       const payload = {
         firstName: "Budi",
         lastName: "Budiman",
@@ -349,20 +401,12 @@ describe("POST /register - Create User", () => {
       const res = await request(app)
         .post("/register")
         .send(payload)
-        .set({ access_token });
+        .set({ access_token: adminToken });
       expect(res.status).toBe(400);
       expect(res.body).toHaveProperty("message", "Please insert password");
     });
 
     it('Should return an object with status of 400 and a message "Please insert user phone number"', async () => {
-      const jwtPayload = {
-        id: 1,
-        email: "heri@dinasq.com",
-        role: "Super Admin",
-      };
-
-      const access_token = signPayload(jwtPayload);
-
       const payload = {
         firstName: "Budi",
         lastName: "Budiman",
@@ -377,7 +421,7 @@ describe("POST /register - Create User", () => {
       const res = await request(app)
         .post("/register")
         .send(payload)
-        .set({ access_token });
+        .set({ access_token: adminToken });
       expect(res.status).toBe(400);
       expect(res.body).toHaveProperty(
         "message",
@@ -386,14 +430,6 @@ describe("POST /register - Create User", () => {
     });
 
     it('Should return an object with status of 400 and a message "Please insert user address"', async () => {
-      const jwtPayload = {
-        id: 1,
-        email: "heri@dinasq.com",
-        role: "Super Admin",
-      };
-
-      const access_token = signPayload(jwtPayload);
-
       const payload = {
         firstName: "Budi",
         lastName: "Budiman",
@@ -408,20 +444,12 @@ describe("POST /register - Create User", () => {
       const res = await request(app)
         .post("/register")
         .send(payload)
-        .set({ access_token });
+        .set({ access_token: adminToken });
       expect(res.status).toBe(400);
       expect(res.body).toHaveProperty("message", "Please insert user address");
     });
 
     it('Should return an object with status of 400 and a message "Please insert user position"', async () => {
-      const jwtPayload = {
-        id: 1,
-        email: "heri@dinasq.com",
-        role: "Super Admin",
-      };
-
-      const access_token = signPayload(jwtPayload);
-
       const payload = {
         firstName: "Budi",
         lastName: "Budiman",
@@ -436,7 +464,7 @@ describe("POST /register - Create User", () => {
       const res = await request(app)
         .post("/register")
         .send(payload)
-        .set({ access_token });
+        .set({ access_token: adminToken });
       expect(res.status).toBe(400);
       expect(res.body).toHaveProperty("message", "Please insert user position");
     });
